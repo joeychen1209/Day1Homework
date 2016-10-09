69262988fb8c03e8f6b8ad005bb0e4ee9b10891a
joey.chen <joeychen1209@gmail.com>
2016-10-10 00:43:28 +0800

    需求:1. 來源可以是「任何型別的集合」 2. 可以任意決定幾筆一組 3. 回傳每一組 Σf(x) 的集合- Σ結果型別可直接用int

---
+++ Day1Homework/cal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Day1Homework
+{
+    public class cal
+    {
+        /// <summary>
+        /// 需求:1. 來源可以是「任何型別的集合」 2. 可以任意決定幾筆一組 3. 回傳每一組 Σf(x) 的集合- Σ結果型別可直接用int
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="CalList">泛型資料</param>
+        /// <param name="CalColName">計算欄位名稱</param>
+        /// <param name="RowCnt">分組筆數</param>
+        /// <returns></returns>
+        public List<int> Sum<T>(List<T> CalList, string CalColName, int RowCnt)
+        {
+            List<int> Ret = new List<int>();
+            int totalpage = Convert.ToInt32(System.Math.Ceiling(Convert.ToDouble(CalList.Count) / Convert.ToDouble(RowCnt)));
+            int pagecnt = 1;
+            int Sum = 0;
+            for (int i = 0; i < totalpage; i++)
+            {
+                List<T> List = CalList.Skip((pagecnt - 1) * RowCnt).Take(RowCnt).ToList();
+                for (int j = 0; j < List.Count(); j++)
+                {
+                    int iSum = 0;
+                    if(int.TryParse(List[j].GetType().GetProperty(CalColName).GetValue(List[j], null).ToString(), out iSum))
+                        Sum += iSum;
+                }
+                Ret.Add(Sum);
+                pagecnt++;
+                Sum = 0;
+            }
+            return Ret.ToList();
+        }
+
+    }
+
+
+}
---
+++ Day1HomeworkTests/calTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ExpectedObjects;
+
+namespace Day1Homework.Tests
+{
+    [TestClass()]
+    public class calTests
+    {
+        [TestMethod()]
+        public void SumTest_3筆一組取Cost總和()
+        {
+
+            var expected = new List<int>() { 6, 15, 24, 21 };
+            var target = new cal();
+            var actual = target.Sum<Prod>(GetData(), "Cost", 3);
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+
+        }
+
+        [TestMethod()]
+        public void SumTest_4筆一組取Revenue總和()
+        {
+
+            var expected = new List<int>() { 50, 66, 60 };
+            var target = new cal();
+            var actual = target.Sum<Prod>(GetData(), "Revenue", 4);
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+
+        }
+
+        internal class Prod
+        {
+            public int Id { set; get; }
+            public float Cost { set; get; }
+            public decimal Revenue { set; get; }
+            public long SellPrice { set; get; }
+        }
+
+        internal List<Prod> GetData()
+        {
+            var data = new List<Prod>
+            {
+                new Prod {Id=1,Cost=1,Revenue=11,SellPrice=21 },
+                new Prod {Id=2,Cost=2,Revenue=12,SellPrice=22 },
+                new Prod {Id=3,Cost=3,Revenue=13,SellPrice=23 },
+                new Prod {Id=4,Cost=4,Revenue=14,SellPrice=24 },
+                new Prod {Id=5,Cost=5,Revenue=15,SellPrice=25 },
+                new Prod {Id=6,Cost=6,Revenue=16,SellPrice=26 },
+                new Prod {Id=7,Cost=7,Revenue=17,SellPrice=27 },
+                new Prod {Id=8,Cost=8,Revenue=18,SellPrice=28 },
+                new Prod {Id=9,Cost=9,Revenue=19,SellPrice=29 },
+                new Prod {Id=10,Cost=10,Revenue=20,SellPrice=30 },
+                new Prod {Id=11,Cost=11,Revenue=21,SellPrice=31 }
+            };
+
+            return data;
+        }
+    }
+}
